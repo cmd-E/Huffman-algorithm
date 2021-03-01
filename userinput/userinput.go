2120d042d7af1e1ec97797e25fc8e02648245e75
Kirill <perevyshink_1@mail.ru>
2021-03-01 21:47:39 +0600

    corrected flags description. Added ability to read help file and print it out

--- userinput/userinput.go
+++ userinput/userinput.go
@@ -3,6 +3,7 @@ package userinput
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 )
 
 var word string
@@ -11,9 +12,9 @@ var printHelp bool
 
 // InitFlags - defines flags for user to operate program
 func InitFlags() {
-	flag.StringVar(&word, "w", "", "User input to encode")
-	flag.StringVar(&customOccurrencesFilePath, "p", "", "File where custom occurrences for all symbols in word are defined")
-	flag.BoolVar(&printHelp, "h", false, "Print help to user")
+	flag.StringVar(&word, "w", "", "Input to encode")
+	flag.StringVar(&customOccurrencesFilePath, "p", "", "File where custom occurrences for all symbols in input are defined")
+	flag.BoolVar(&printHelp, "h", false, "Print help")
 }
 
 // GetData - returns user input and path to file with custom occurrences
@@ -28,6 +29,10 @@ func GetHelp() bool {
 
 // PrintHelp - prints help if user is asking for it
 func PrintHelp() {
-	// TODO print help from file
-	fmt.Println("Help is on the way!")
+	helpFile, err := ioutil.ReadFile("help.txt")
+	if err != nil {
+		fmt.Println("There was an error while attempting to read file text: ", err.Error())
+		return
+	}
+	fmt.Println(string(helpFile))
 }
